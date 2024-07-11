5d8f42325d0ac4f2c034b39db060e2e5e7cfb235
phamnga-223 <pnga2231997@gmail.com>
2024-07-11 19:58:27 +0700

    Update bai tap cau 4

--- src/baiTap/Cau4.java
+++ src/baiTap/Cau4.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class Cau4 {
 
+	public static final int N = 5;
+	
 	public static void main (String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		int n = 5;		
 		int a = 0;
 		System.out.println("Nhập vào 5 số: ");
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < N; i++) {
 			a += scanner.nextInt();
 		}
-		int trungBinh = (int)a/n;
+		int trungBinh = (int)a/N;
 		
 		String ketQua = "Trung bình là: " + trungBinh;
 		System.out.println(ketQua);
